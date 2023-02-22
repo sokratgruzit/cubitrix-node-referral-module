5dfab3e65c6cb33c0597b15cf767916898b0d04c
MarkoPolo88 <talakhadzemariam@gmail.com>
2023-02-22 15:27:20 +0400

    simple change

--- controllers/referrals_controller.js
+++ controllers/referrals_controller.js
@@ -2,13 +2,12 @@ const { accounts } = require("@cubitrix/models");
 const main_helper = require("../helpers/index");
 
 // get account balance
-async function test_function(address, account_type_id) {
+async function test_function(req, res) {
   try {
-    return main_helper.return_data(true, "ko");
-    return main_helper.error_message("error");
+    return main_helper.success_response(res, "success");
   } catch (e) {
     console.log(e.message);
-    return main_helper.error_message("error");
+    return main_helper.error_response(res, "error");
   }
 }
 
