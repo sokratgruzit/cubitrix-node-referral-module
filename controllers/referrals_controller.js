83670ced411dde5f691bc4be48669f93b88e9439
MarkoPolo88 <talakhadzemariam@gmail.com>
2023-02-25 18:18:18 +0400

    methods for api:

--- controllers/referrals_controller.js
+++ controllers/referrals_controller.js
@@ -3,52 +3,6 @@ const main_helper = require("../helpers/index");
 const global_helper = require("../helpers/global_helper");
 const shortid = require("shortid");
 
-// get account balance
-async function test_function(req, res) {
-  try {
-    let {
-      type,
-      address,
-      referral,
-      referral_avtivated,
-      referral_uni_percentage,
-      referral_binary_lvl1_percentage,
-      referral_binary_lvl2_percentage,
-    } = req.body;
-    let return_data;
-    if (type == "generate_referral_code") {
-      return_data = {
-        uni: await generate_referral_code(),
-        binary: await generate_referral_code(),
-      };
-    } else if (type == "bind_referral_to_user") {
-      let referral_codes = {
-        uni: await generate_referral_code(),
-        binary: await generate_referral_code(),
-      };
-      return_data = await bind_referral_to_user(address, referral_codes);
-    } else if (type == "get_referral_by_code") {
-      return_data = await get_referral_by_code(referral);
-    } else if (type == "get_referral_by_address") {
-      return_data = await get_referral_by_address(address);
-    } else if (type == "assign_refferal_to_user") {
-      return_data = await assign_refferal_to_user(referral, address);
-    } else if (type == "admin_setup") {
-      return_data = await admin_setup(
-        referral_avtivated,
-        referral_uni_percentage,
-        referral_binary_lvl1_percentage,
-        referral_binary_lvl2_percentage
-      );
-    } else if (type == "get_referral_options") {
-      return_data = await get_referral_options();
-    }
-    return main_helper.success_response(res, return_data);
-  } catch (e) {
-    console.log(e.message);
-    return main_helper.error_response(res, "error");
-  }
-}
 const generate_referral_code = async () => {
   // Generate a short, unique ID with the prefix "REF_"
   let ref_code = "REF_" + shortid.generate() + shortid.generate();
@@ -58,44 +12,7 @@ const generate_referral_code = async () => {
   }
   return ref_code;
 };
-const bind_referral_to_user = async (address, referrals) => {
-  try {
-    let check_by_address = await get_referral_by_address(address);
-    if (check_by_address.length > 0) {
-      return "referrals on this address already exists";
-    }
-    for (let i = 0; i < referrals.length; i++) {
-      let check_by_code_uni = await get_referral_by_code(referrals[i].uni);
-      if (check_by_code_uni.length > 0) {
-        return "referral uni code already used";
-      }
-      let check_by_code_binary = await get_referral_by_code(
-        referrals[i].binary
-      );
-      if (check_by_code_binary.length > 0) {
-        return "referral binary code already used";
-      }
-    }
 
-    let account_id = await global_helper.get_account_by_address(address);
-    if (account_id) {
-      for (var k in referrals) {
-        await referral_links.create({
-          account_id,
-          referral: referrals[k],
-          referral_type: k,
-        });
-      }
-      let referral_links_in_db = await referral_links.find({ account_id });
-      return referral_links_in_db;
-    } else {
-      return "error saving referral code to user";
-    }
-  } catch (e) {
-    console.log(e.message);
-    return e.message;
-  }
-};
 const get_referral_by_code = async (referral) => {
   try {
     let code_exists = await referral_links.find({
@@ -138,12 +55,16 @@ const user_already_have_referral_code = async (user_id) => {
     return false;
   }
 };
-const assign_refferal_to_user = async (referral, address) => {
+const assign_refferal_to_user = async (req, res) => {
   try {
+    let { referral, address } = req.body;
     let user_id = await global_helper.get_account_by_address(address);
     let check_ref = await user_already_have_referral_code(user_id);
     if (check_ref) {
-      return "User already activated referral code";
+      return main_helper.error_response(
+        res,
+        "User already activated referral code"
+      );
     }
     let referral_id = await global_helper.get_referral_by_code(referral);
     let assign_ref_to_user = await referral_uni_users.create({
@@ -152,22 +73,23 @@ const assign_refferal_to_user = async (referral, address) => {
       referral,
     });
     if (assign_ref_to_user) {
-      return assign_ref_to_user;
+      return main_helper.success_response(res, assign_ref_to_user);
     } else {
-      return "error";
+      return main_helper.error_response(res, "error");
     }
   } catch (e) {
     console.log(e.message);
-    return e.message;
+    return main_helper.error_response(res, "error");
   }
 };
-const admin_setup = async (
-  referral_avtivated,
-  referral_uni_percentage,
-  referral_binary_lvl1_percentage,
-  referral_binary_lvl2_percentage
-) => {
+const admin_setup = async (req, res) => {
   try {
+    let {
+      referral_avtivated,
+      referral_uni_percentage,
+      referral_binary_lvl1_percentage,
+      referral_binary_lvl2_percentage,
+    } = req.body;
     let referral_options = {
       referral_avtivated: referral_avtivated,
       referral_uni_percentage: parseFloat(referral_uni_percentage),
@@ -182,27 +104,129 @@ const admin_setup = async (
       "referral_options",
       referral_options
     );
-    return update;
+    return main_helper.success_response(res, update);
   } catch (e) {
     console.log(e.message);
-    return;
+    return main_helper.error_response(res, "error");
   }
 };
-const get_referral_options = async () => {
+const get_referral_options = async (req, res) => {
   try {
     let get_referral_options = await global_helper.get_option_by_key(
       "referral_options"
     );
     if (get_referral_options.success) {
-      return get_referral_options?.data?.object_value;
+      return main_helper.success_response(
+        res,
+        get_referral_options?.data?.object_value
+      );
     } else {
-      return {};
+      return main_helper.success_response(res, {});
     }
   } catch (e) {
     console.log(e.message);
-    return;
+    main_helper.error_response(res, "error");
   }
 };
+const generate_referral_codes = async (req, res) => {
+  try {
+    let return_data = {
+      uni: await generate_referral_code(),
+      binary: await generate_referral_code(),
+    };
+    return main_helper.success_response(res, return_data);
+  } catch (e) {
+    return main_helper.error_response(res, "error");
+  }
+};
+
+const bind_referral_to_user = async (req, res) => {
+  try {
+    let { address } = req.body;
+    let referrals = {
+      uni: await generate_referral_code(),
+      binary: await generate_referral_code(),
+    };
+    let check_by_address = await get_referral_by_address(address);
+    if (check_by_address.length > 0) {
+      return main_helper.error_response(
+        res,
+        "referrals on this address already exists or address is invalid"
+      );
+    }
+    for (let i = 0; i < referrals.length; i++) {
+      let check_by_code_uni = await get_referral_by_code(referrals[i].uni);
+      if (check_by_code_uni.length > 0) {
+        return main_helper.error_response(
+          res,
+          "referral uni code already used"
+        );
+      }
+      let check_by_code_binary = await get_referral_by_code(
+        referrals[i].binary
+      );
+      if (check_by_code_binary.length > 0) {
+        return main_helper.error_response(
+          res,
+          "referral binary code already used"
+        );
+      }
+    }
+
+    let account_id = await global_helper.get_account_by_address(address);
+    if (account_id) {
+      for (var k in referrals) {
+        await referral_links.create({
+          account_id,
+          referral: referrals[k],
+          referral_type: k,
+        });
+      }
+      let referral_links_in_db = await referral_links.find({ account_id });
+      return main_helper.success_response(res, referral_links_in_db);
+    } else {
+      return main_helper.error_response(
+        res,
+        "error saving referral code to user"
+      );
+    }
+  } catch (e) {
+    console.log(e.message);
+    return main_helper.error_response(res, e.message);
+  }
+};
+
+const get_referrals_by_address = async (req, res) => {
+  try {
+    let { address } = req.body;
+    let return_data;
+    return_data = await get_referral_by_address(address);
+
+    return main_helper.success_response(res, return_data);
+  } catch (e) {
+    console.log(e.message);
+    return main_helper.error_response(res, "error");
+  }
+};
+const get_referrals_by_code = async (req, res) => {
+  try {
+    let { referral } = req.body;
+    let return_data;
+    return_data = await get_referral_by_code(referral);
+
+    return main_helper.success_response(res, return_data);
+  } catch (e) {
+    console.log(e.message);
+    return main_helper.error_response(res, "error");
+  }
+};
+
 module.exports = {
-  test_function,
+  get_referral_options,
+  generate_referral_codes,
+  bind_referral_to_user,
+  get_referrals_by_address,
+  get_referrals_by_code,
+  assign_refferal_to_user,
+  admin_setup,
 };
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,27 @@ const referral_controller = require("../controllers/referrals_controller");
 const cookieParser = require("cookie-parser");
 
 router.use(cookieParser());
-router.post("/test", referral_controller.test_function);
+router.get("/get_referral_options", referral_controller.get_referral_options);
+router.get(
+  "/generate_referral_codes",
+  referral_controller.generate_referral_codes
+);
+router.post(
+  "/bind_referral_to_user",
+  referral_controller.bind_referral_to_user
+);
+router.post(
+  "/get_referrals_by_address",
+  referral_controller.get_referrals_by_address
+);
+router.post(
+  "/get_referrals_by_code",
+  referral_controller.get_referrals_by_code
+);
+router.post(
+  "/assign_refferal_to_user",
+  referral_controller.assign_refferal_to_user
+);
+router.post("/admin_setup", referral_controller.admin_setup);
+
 module.exports = router;
