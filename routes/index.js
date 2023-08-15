1eb05f5ac4f2e6ee3c79921e10f7862f3d47f8b8
gogaokradze <deathofsuper@gmail.com>
2023-08-15 13:38:19 +0400

    fix

--- routes/index.js
+++ routes/index.js
@@ -11,29 +11,23 @@ router.use(cookieParser());
 router.post("/register_referral", referral_controller.register_referral);
 router.post("/get_referral_data", referral_controller.get_referral_data);
 router.post("/get_referral_tree", referral_controller.get_referral_tree);
-router.post(
-  "/get_referral_data_uni",
-  referral_controller.get_referral_data_uni
-);
+router.post("/get_referral_data_uni", referral_controller.get_referral_data_uni);
 
 router.post("/get_referral_code", referral_controller.get_referral_code);
 router.post(
   "/get_referral_uni_transactions",
-  referral_controller.get_referral_uni_transactions
+  referral_controller.get_referral_uni_transactions,
 );
 
 router.post(
   "/get_referral_binary_transactions",
-  referral_controller.get_referral_binary_transactions
+  referral_controller.get_referral_binary_transactions,
 );
-router.post(
-  "/get_reerral_global_data",
-  referral_controller.get_reerral_global_data
-);
+router.post("/get_reerral_global_data", referral_controller.get_reerral_global_data);
 router.post("/get_referral_address", referral_controller.get_referral_address);
 router.post(
   "/get_referral_parent_address",
-  referral_controller.get_referral_parent_address
+  referral_controller.get_referral_parent_address,
 );
 router.post("/get_referral_options", referral_controller.get_referral_options);
 router.post("/binary_comission_count_user", async (req, res) => {
@@ -41,10 +35,9 @@ router.post("/binary_comission_count_user", async (req, res) => {
   const currentDate = new Date();
   const currentDayOfMonth = currentDate.getDate();
   const daysPassed = currentDayOfMonth - 1;
-  console.log(daysPassed);
   let results = await referral_controller.binary_comission_count_user(
     daysPassed,
-    address
+    address,
   );
   res.status(200).json({ results });
 });
